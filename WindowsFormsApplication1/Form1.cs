5e43bb0e77fbee7c96ecef775610f12c97f0a992
akindyni-masaripayments <akindyni.michael@masaripayments.com>
2016-11-04 16:42:11 +0200

    test button

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,9 +19,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //nothing yet
-            //vkrkm
-            System.Console.WriteLine("Hello World");
+            
         }
     }
 }
